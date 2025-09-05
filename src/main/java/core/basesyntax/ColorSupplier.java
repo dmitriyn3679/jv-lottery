4a823922bd9b5a957d8de972dd028210be0d2a9a
DmytroNikolenko <dmitriyn3679@gmail.com>
2025-09-05 10:02:40 +0300

    done

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public static String getRandomColor() {
+        Random random = new Random();
+
+        int colorIdx = random.nextInt(Color.values().length);
+        Color color = Color.values()[colorIdx];
+
+        return color.toString();
     }
 }
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public static Ball getRandomBall() {
+        Random random = new Random();
+
+        String ballColor = ColorSupplier.getRandomColor();
+        int ballNumber = random.nextInt(100);
+
+        return new Ball(ballColor, ballNumber);
+    }
+}
